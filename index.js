9d5cb0e8df69e19d426d728a928da3aee80b1704
nggigi <ngigi.francis5@gmail.com>
2024-01-05 12:08:15 -0500

    done

---
+++ index.js
@@ -0,0 +1,36 @@
+ let number = 5;
+ let numberTwo= 10;
+
+function add(){
+    return number+=3
+}
+
+function subtract(){
+    return number-=2
+}
+
+function multiply(){
+    return number *=10
+}
+
+function divide(){
+    return  number /=5
+}
+function add( a,b){
+    return a + b
+}
+add(number,numberTwo)
+
+function subtract( a,b){
+    return b-a
+}
+subtract(number,numberTwo)
+
+function multiply( a, b){
+    return  a*b}
+    multiply(number,numberTwo)
+
+    function divide( a,b){
+        return b/a
+    }
+    divide(number,numberTwo)
